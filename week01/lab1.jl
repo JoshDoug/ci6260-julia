32d3642e3155ce06269014bea60f403352655f22
Joshua Stringfellow <joshdougstringfellow@gmail.com>
2018-05-31 02:20:52 +0100

    Add week 1 tasks outline

---
+++ week01/lab1.jl
@@ -0,0 +1,7 @@
+# Week 1 Tasks
+
+## Task 1 - Create and plot a simple graphical object (e.g. a rectangle)
+
+## Task 2 - Convert script to use a function, 'rect', that plots a rectangle given x, y, width, and height parameters
+
+## Task 3 - Draw a house using lines and the 'rect' function
