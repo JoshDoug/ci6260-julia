d32c2289c6f7e83e7e26564d542e0cf46070fb4b
Joshua Stringfellow <joshdougstringfellow@gmail.com>
2018-06-08 21:01:07 +0100

    Add task 1 - plot rect

--- week01/lab1.jl
+++ week01/lab1.jl
@@ -1,7 +1,15 @@
 # Week 1 Tasks
 
 ## Task 1 - Create and plot a simple graphical object (e.g. a rectangle)
+using Plots
+gr()
+rect = [[0, 2, 2, 0], [0, 0, 2, 2]]
+plt = plot(rect[1], rect[2], label="line")
+display(plt)
+
+##
 
 ## Task 2 - Convert script to use a function, 'rect', that plots a rectangle given x, y, width, and height parameters
 
 ## Task 3 - Draw a house using lines and the 'rect' function
+#x = 5
